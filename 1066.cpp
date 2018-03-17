96dd9fb0430c422b1e490092e67aa93574e01374
yy <641016261@qq.com>
2018-03-18 03:33:01 +0800

    finished 1066

--- 1066.cpp
+++ 1066.cpp
@@ -17,31 +17,109 @@
 
 using namespace std;
 
-const int maxn = 101;
 
 struct node {
-	int data;
-	int lchild;
-	int rchild;
-} tree[maxn];
-int n;
+	int v, height;
+	node* lchild;
+	node* rchild;
+};
 
-int number[maxn];
+node* newNode(int v) {
+	node* Node = new node;
+	Node->v = v;
+	Node->height = 1;
+	Node->lchild = Node->rchild = NULL;
+	return Node;
+}
 
-int index = 0, num = 0;
+int getHeight(node* root) {
+	if (root == NULL) {
+		return 0;
+	}
+	return root->height;
+}
+
+void updateHeight(node* root) {
+	root->height = max(getHeight(root->lchild), getHeight(root->rchild)) + 1;
+}
+
+int getBalanceFactor(node* root) {
+	return getHeight(root->lchild) - getHeight(root->rchild);
+}
+
+void L(node* &root) {
+	node* temp;
+	temp = root->rchild;
+	root->rchild = temp->lchild;
+	temp->lchild = root;
+	updateHeight(root);
+	updateHeight(temp);
+	root = temp;
+}
+
+void R(node* &root) {
+	node* temp;
+	temp = root->lchild;
+	root->lchild = temp->rchild;
+	temp->rchild = root;
+	updateHeight(root);
+	updateHeight(temp);
+	root = temp;
+}
 
 
+void insert(node* &root, int v) {
+	if (root == NULL) {
+		root = newNode(v);
+		return;
+	}
+	if (v < root->v) {
+		insert(root->lchild, v);
+		updateHeight(root);
+		if (getBalanceFactor(root) == 2) {
+			if (getBalanceFactor(root->lchild) == 1) {
+				R(root);
+			}
+			else if (getBalanceFactor(root->lchild) == -1) {
+				L(root->lchild);
+				R(root);
+			}
+		}
+	}
+	else {
+		insert(root->rchild, v);
+		updateHeight(root);
+		if (getBalanceFactor(root) == -2) {
+			if (getBalanceFactor(root->rchild) == -1) {
+				L(root);
+			}
+			else if (getBalanceFactor(root->rchild) == 1) {
+				R(root->rchild);
+				L(root);
+			}
+		}
+	}
+}
+
+node* Creat(int data[], int n) {
+	node* root = NULL;
+	for (int i = 0; i < n; i++) {
+		insert(root, data[i]);
+	}
+	return root;
+}
 
 int main() {
-	int n;
+	int n, v;
 	scanf("%d", &n);
+	int num[20];
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &number[i]);
+		scanf("%d", &v);
+		num[i] = v;
 	}
+	node* root = Creat(num, n);
+	printf("%d", root->v);
 
-	sort(number, number + n);
-	int mid = n / 2 + n % 2 - 1;
-	printf("%d",number[mid]);
 	system("pause");
 	return 0;
 }
